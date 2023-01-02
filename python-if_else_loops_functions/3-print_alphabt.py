f11cedb986a7493b54bb910db81868cbe5127434
root <anithav@brightcomgroup.com>
2023-01-01 23:01:42 -0800

    3 task

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for letter_code in range(ord('a'), ord('z')+1):
-    if chr(letter_code) not in "qe":
-        print(chr(letter_code), end="")
+for alpha_letters in range(ord('a'), ord('z')+1):
+    if alpha_letters == ord('e') or alpha_letters == ord('q'):
+       continue
+    print("{:c}".format(alpha_letters), end="")
