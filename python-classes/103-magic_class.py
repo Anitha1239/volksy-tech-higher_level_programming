7c67ba8ecb63e9a29eec5ffb03cc83a786b73281
root <anithav@brightcomgroup.com>
2023-01-12 02:21:06 -0800

    anitha

--- python-classes/103-magic_class.py
+++ python-classes/103-magic_class.py
@@ -6,7 +6,6 @@ import math
 class MagicClass:
     def __init__(self, radius=0):
         """Init docstring"""
-        
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
                 raise TypeError('radius must be a number')
