d68dc3c0b799bf06cca487a91a28460e0f8cc2c8
root <anithav@brightcomgroup.com>
2023-02-02 09:24:12 -0800

    16

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- hii
+SELECT tv_shows.title, tv_genres.name FROM tv_show_genres RIGHT OUTER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id LEFT OUTER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title ASC, tv_genres.name ASC;
---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+-- sql
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California");
